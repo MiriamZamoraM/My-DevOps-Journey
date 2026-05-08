1abe282b28e27456a8d4c2433d082cf0d15e5f9e
MiriamZamoraM <ada_hack410@hackademy.mx>
2026-05-08 17:31:27 -0600

    Starting with Docker

---
+++ docker/second/hello.py
@@ -0,0 +1,7 @@
+print("Hello-World from Python \nMy name is Miriam")
+
+import time
+
+print("Contenedor iniciado... soy relentless.")
+while True:
+    time.sleep(60) # Se duerme un minuto y repite, así no gasta CPU pero no muere
